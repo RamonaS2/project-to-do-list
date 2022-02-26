1735584df27c35ab655016aaf2f6d6dc723e04a7
RamonaS2 <bandeiraramona2@gmail.com>
2022-02-26 20:29:26 -0300

    requisito 5 e 6

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
- 
+let botao = document.querySelector('#criar-tarefa');
+
+
+botao.addEventListener('click', function(){
+
+   let inputValue = document.querySelector("#texto-tarefa").value;
+   let itemList = document.createElement('li');
+   
+   itemList.innerText = inputValue;
+
+   let list = document.querySelector("#lista-tarefas");
+   
+   list.appendChild(itemList); 
+
+   let input = document.querySelector("#texto-tarefa");
+   input.value = '';
+   
+} 
+)
