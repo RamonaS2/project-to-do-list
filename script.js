edbdb39a2dd6b0fb31ace078d467743b0abc2327
RamonaS2 <bandeiraramona2@gmail.com>
2022-03-07 11:34:38 -0300

    requisito 8 e 12

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const list = document.querySelector('#lista-tarefas');
 botao.addEventListener('click', function () {
   let inputValue = document.querySelector('#texto-tarefa').value;
   let itemList = document.createElement('li');
-  itemList.className ='item-list';
+  itemList.className = 'item-list';
 
   itemList.innerText = inputValue;
 
@@ -15,13 +15,16 @@ botao.addEventListener('click', function () {
 });
 
 function backgroundLine(event) {
-   let line = document.getElementsByTagName('li')
-   for (let index = 0; index < line.length; index++) {
-      line[index] = event.target.style.backgroundColor = 'gray';
-       
-   }
+ 
+ let classSelected = document.getElementsByClassName('selected')[0];
+ console.log(classSelected);
+ if(classSelected){
+   classSelected.classList.remove('selected');
+ }
+ event.target.classList.toggle('selected');
+ 
+ // event.target.style.backgroundColor = 'gray';
 }
-
 list.addEventListener('click', backgroundLine);
 
 function classLineColor(event) {
@@ -33,43 +36,90 @@ list.addEventListener('dblclick', classLineColor);
 let botaoApaga = document.querySelector('#apaga-tudo');
 
 botaoApaga.addEventListener('click', function () {
-   let line = document.getElementsByTagName('li');
-   
-   for (let index = 0; index < line.length; index+= 0) {
-      
-      list.removeChild(line[index]);
-   }
-   
-})
+  let line = document.getElementsByTagName('li');
 
-let botaoFinalizados = document.querySelector("#remover-finalizados");
+  for (let index = 0; index < line.length; index += 0) {
+    list.removeChild(line[index]);
+  }
+});
+
+let botaoFinalizados = document.querySelector('#remover-finalizados');
 
 botaoFinalizados.addEventListener('click', function () {
-   let lineCompleted = document.getElementsByClassName('completed');
+  let lineCompleted = document.getElementsByClassName('completed');
 
-   for (let index = 0; index < lineCompleted.length; index+= 0) {
-     
-      list.removeChild(lineCompleted[index]);
-   }
+  for (let index = 0; index < lineCompleted.length; index += 0) {
+    list.removeChild(lineCompleted[index]);
+  }
+});
 
-})
+let botaoSelectRemove = document.querySelector('#remover-selecionado');
 
-
-
-let botaoSelectRemove = document.querySelector("#remover-selecionado");
-
-function selectItens (event){
-event.target.classList.toggle('selecionados');
+function selectItens(event) {
+  event.target.classList.toggle('selecionados');
 }
 
 list.addEventListener('click', selectItens);
 
-function removeItens () {
-let lineSelect = document.getElementsByClassName('selecionados');
+function removeItens() {
+  let lineSelect = document.getElementsByClassName('selecionados');
 
-for(let i = 0; i < lineSelect.length; i += 0){
-   list.removeChild(lineSelect[i]);
-}
+  for (let i = 0; i < lineSelect.length; i += 0) {
+    list.removeChild(lineSelect[i]);
+  }
 }
 
-botaoSelectRemove.addEventListener('click', removeItens); 
+botaoSelectRemove.addEventListener('click', removeItens);
+
+// REQUISITO 12 - SALVAR LISTA
+
+let botaoSalva = document.querySelector('#salvar-tarefas');
+
+botaoSalva.addEventListener('click', function () {
+  localStorage.setItem('sList', list.innerHTML);
+
+});
+
+function listaSalva() {
+  let listS = localStorage.getItem('sList');
+
+  list.innerHTML = listS;
+
+}
+
+window.onload = listaSalva ();
+
+//REQUISITO 13
+// MOVER PARA CIMA
+let botaoMoveUp = document.querySelector('#mover-cima');
+
+function selectItemUp(event) {
+  event.target.classList.toggle('paraCima');
+}
+
+list.addEventListener('click', selectItemUp);
+
+function moveUp() {
+  let itemUp = document.querySelector('.paraCima');
+  let itemAnterior = itemUp.previousElementSibling;
+
+  list.insertBefore(itemAnterior, itemUp.nextElementSibling);
+}
+
+botaoMoveUp.addEventListener('click', moveUp);
+
+//MOVER PARA BAIXO
+let botaoMoveDown = document.querySelector('#mover-baixo');
+
+function selectItemDown(event) {
+  event.target.classList.toggle('paraBaixo');
+}
+list.addEventListener('click', selectItemDown);
+
+function moveDown() {
+  let itemDown = document.querySelector('.paraBaixo');
+  let proximoItem = itemDown.nextElementSibling;
+
+  list.insertBefore(proximoItem, itemDown.previousElementSibling);
+}
+botaoMoveDown.addEventListener('click', moveDown);
